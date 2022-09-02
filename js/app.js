14fcba28662af3f830551234d5ca256af0553da9
Nicolas Quinn <nclsquinn@gmail.com>
2022-09-01 23:42:36 -0300

    127 - Validar los campos

--- js/app.js
+++ js/app.js
@@ -2,12 +2,30 @@
 
 const btnEnviar = document.querySelector('#enviar');
 
+const inputEmail = document.querySelector('#email');
+const inputAsunto = document.querySelector('#asunto');
+const inputMensaje = document.querySelector('#mensaje');
+
 cargarEventListeners();
 function cargarEventListeners () {
+
     document.addEventListener('DOMContentLoaded', iniciarApp);
+
+    inputEmail.addEventListener('blur', validarFormulario);
+    inputAsunto.addEventListener('blur', validarFormulario);
+    inputMensaje.addEventListener('blur', validarFormulario);
 }
 
 function iniciarApp() {
     btnEnviar.disabled = true;
     btnEnviar.classList.add('opacity-50', 'opacity-50');
+}
+
+function validarFormulario (e) {
+    if (e.target.value.length > 0) {
+        console.log("Hay datos")
+        e.target.classList.remove('formulario-vacio');
+    } else {
+        e.target.classList.add('formulario-vacio');
+    }
 }
