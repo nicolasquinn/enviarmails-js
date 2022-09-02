4e0301418cff939a2fb53591ec10221cd4148dd2
Nicolas Quinn <nclsquinn@gmail.com>
2022-09-02 10:48:50 -0300

    128 - Limpiar los mensajes de error

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ const btnEnviar = document.querySelector('#enviar');
 const inputEmail = document.querySelector('#email');
 const inputAsunto = document.querySelector('#asunto');
 const inputMensaje = document.querySelector('#mensaje');
+const formulario = document.querySelector('#enviar-mail');
 
 cargarEventListeners();
 function cargarEventListeners () {
@@ -18,7 +19,7 @@ function cargarEventListeners () {
 
 function iniciarApp() {
     btnEnviar.disabled = true;
-    btnEnviar.classList.add('opacity-50', 'opacity-50');
+    btnEnviar.classList.add('opacity-50', 'cursor-not-allowed');
 }
 
 function validarFormulario (e) {
@@ -27,5 +28,18 @@ function validarFormulario (e) {
         e.target.classList.remove('formulario-vacio');
     } else {
         e.target.classList.add('formulario-vacio');
+        mostrarError();
     }
+}
+
+function mostrarError() {
+    const mensajeError = document.createElement('P');
+    mensajeError.textContent = "Tienes que rellenar todos los campos.";
+    mensajeError.classList.add('mensaje-error', 'error');
+
+    const errores = document.querySelectorAll('.error')
+    if (errores.length === 0) {
+        formulario.appendChild(mensajeError);
+    }
+
 }
